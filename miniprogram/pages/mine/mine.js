692c9f514899324951631af6ee77e3b6e95d42e7
kevinjiang <494371860@qq.com>
2022-11-27 23:05:12 +0800

    增加个人页面的dialog

--- miniprogram/pages/mine/mine.js
+++ miniprogram/pages/mine/mine.js
@@ -4,6 +4,7 @@ const app = getApp();
 
 const db = wx.cloud.database()
 const userInfoCollection = db.collection('userInfo')
+const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
 const suggestInfoCollection = db.collection('suggestInfo')
 const com = db.command
 const _ = db.command
@@ -28,6 +29,10 @@ Page({
         }, {
             text: '提交'
         }],
+        avatarUrl: defaultAvatarUrl,
+        theme: wx.getSystemInfoSync().theme,
+        nickName: '',
+        typeS: false,
     },
     tapAbout(e) {
         this.setData({
@@ -35,9 +40,10 @@ Page({
         })
     },
     onTapToProfile: function () {
-        wx.navigateTo({
-            url: '../profile/profile',
-        })
+      this.setData({typeS: true})
+        // wx.navigateTo({
+        //     url: '../profile/profile',
+        // })
     },
     tapFeedback(e) {
         const _btn = e.detail.item.text;
@@ -192,6 +198,50 @@ Page({
                 }
             })
         }
-    }
-
+    },
+    onChooseAvatar(e) {
+      const { avatarUrl } = e.detail 
+      this.setData({
+        avatarUrl,
+      })
+    },
+  buttontap(e) {
+    console.log(e.detail)
+  },
+  inputNickname: function (e) {
+      if ((e.detail.nickName) !== '') {
+          this.setData({
+              nickName: e.detail.value
+          });
+          console.log(e);
+      }
+  },
+  inputMajor: function (e) {
+      if ((e.detail.major) !== '') {
+          this.setData({
+              major: e.detail.value
+          });
+          console.log(e);
+      }
+  },
+  submitProfile: function (e) {
+          var time = new Date();
+          console.log(this.data)
+          userInfoCollection.add({
+              data: {
+                  userAvatarUrl: this.data.avatarUrl,
+                  userNickName: this.data.nickName,
+                  lastLoginTime: time,
+                  loginNum: 1,
+                  userOpenId: app.globalData.userOpenId,
+                  userOriginCode: app.globalData.userOriginCode,
+              },
+              success: res => {
+                      app.globalData.userinfo.avatarUrl = e.detail.userInfo.avatarUrl,
+                      app.globalData.userinfo.nickName = e.detail.userInfo.nickName,
+                      app.globalData.userinfo.isAuthed = true;
+                      this.setData({typeS: false})
+              }
+          })
+  }
 })
